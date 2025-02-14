51ea94a9d857be476bbd18f4c08aa43a1542627a
Chris Azzara <cazzara@asu.edu>
2025-02-13 20:20:55 -0500

    2024 day8 part2 solution

---
+++ 2024/day08/part2/solution.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "utils.h"
+
+
+static uint64_t MAX_ROW;
+static uint64_t MAX_COL;
+
+void buildAntennaLocations(std::string& filename, std::unordered_map<char, std::vector<Point>>& antennaLocations)
+{
+    std::ifstream inp = openFile(filename);
+    std::string line;
+    int row = 0;
+    while (std::getline(inp, line))
+    {
+        for (int col = 0; col < line.size(); col++)
+        {
+            if (line[col] != '.')
+            {
+                Point p = {row, col};
+                antennaLocations[line[col]].push_back(p);
+                MAX_COL = line.size();
+            }
+        }
+        row++;
+    }
+    MAX_ROW = row;
+}
+
+std::vector<Point> getAntinodes(Point& a, Point& b)
+{
+    std::vector<Point> points{a, b};
+    Point c = subtractPoints(a, b);
+    Point an1 = addPoints(a, c);
+    while (isPointInBounds(an1, MAX_ROW, MAX_COL))
+    {
+        points.push_back(an1);
+        an1 = addPoints(an1, c);
+    }
+    Point an2 = subtractPoints(b, c);
+    while (isPointInBounds(an2, MAX_ROW, MAX_COL))
+    {
+        points.push_back(an2);
+        an2 = subtractPoints(an2, c);
+    }
+    return points;
+}
+
+
+
+int main()
+{
+    std::string filename = "../test_input.txt";
+    std::unordered_map<char, std::vector<Point>> antennaLocations;
+    std::unordered_set<Point, PointHash, PointEqual> antinodes;
+    buildAntennaLocations(filename, antennaLocations);
+    for (const auto& pair : antennaLocations)
+    {
+        char antenna = pair.first;
+        std::vector<Point> locations = pair.second;
+        for (size_t i = 0; i < locations.size(); i++)
+        {
+            for (size_t j = i + 1; j < locations.size(); j++)
+            {
+                std::vector<Point> potentialAntinodes = getAntinodes(locations[i], locations[j]);
+                for (auto& node : potentialAntinodes)
+                {
+                    antinodes.insert(node);
+                }
+            }
+        }
+    }
+    std::cout << "Found " << antinodes.size() << " antinodes\n";
+    return 0;
+}
